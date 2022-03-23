70c0d4ac9369af9398dd8ab543145ea5b4a59d23
Otilia Vodnitchi <otilia.vod@gmail.com>
2022-03-23 16:32:51 +0200

    added guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 import { InboxModule } from './inbox/inbox.module';
 
 const routes: Routes = [
   {
     path: 'inbox',
+    canLoad: [AuthGuard],
     loadChildren: () =>
       import('./inbox/inbox.module').then((mod) => mod.InboxModule),
   },
