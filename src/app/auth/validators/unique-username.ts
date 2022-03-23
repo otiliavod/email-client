8bc9cb7e259563f636e51b873df9c4486473f322
Otilia Vodnitchi <otilia.vod@gmail.com>
2022-03-23 11:18:17 +0200

    created auth service & reusable input

--- src/app/auth/validators/unique-username.ts
+++ src/app/auth/validators/unique-username.ts
@@ -1,6 +1,6 @@
 import { AsyncValidator, FormControl } from '@angular/forms';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { AuthService } from '../auth.service';
 import { map, catchError } from 'rxjs/operators';
 import { of } from 'rxjs';
 
@@ -9,25 +9,21 @@ export class UniqueUsername implements AsyncValidator {
   validate = (control: FormControl) => {
     const { value } = control;
 
-    return this.http
-      .post<any>('https://api.angular-email.com/auth/username', {
-        username: value,
+    return this.authService.usernameAvailable(value).pipe(
+      map((value) => {
+        if (value.available) {
+          return null;
+        }
+      }),
+      catchError((err) => {
+        if (err.error.username) {
+          return of({ nonUniqueUsername: true });
+        } else {
+          return of({ noConnection: true });
+        }
       })
-      .pipe(
-        map((value) => {
-          if (value.available) {
-            return null;
-          }
-        }),
-        catchError((err) => {
-          if (err.error.username) {
-            return of({ nonUniqueUsername: true });
-          } else {
-            return of({ noConnection: true });
-          }
-        })
-      );
+    );
   };
 
-  constructor(private http: HttpClient) {}
+  constructor(private authService: AuthService) {}
 }
